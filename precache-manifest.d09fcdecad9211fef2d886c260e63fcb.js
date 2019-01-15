4db5e9052220615b9b69a896d24b173dd944f21d
Denis Goncharov <gonchds@gmail.com>
2019-01-15 22:58:50 +0300

    Add files via upload

---
+++ precache-manifest.d09fcdecad9211fef2d886c260e63fcb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "4405193ba65a56b479ef9dfdf5026cf5",
+    "url": "/rss-game/static/media/sprite.4405193b.png"
+  },
+  {
+    "revision": "3d7f26a876a572f30b0f",
+    "url": "/rss-game/static/css/main.40793163.chunk.css"
+  },
+  {
+    "revision": "d8ce377e47732568c515",
+    "url": "/rss-game/static/js/1.d8ce377e.chunk.js"
+  },
+  {
+    "revision": "64b5a9c7a5061954edd3",
+    "url": "/rss-game/static/js/runtime~main.64b5a9c7.js"
+  },
+  {
+    "revision": "dce403994eaa048d0d9a4cca885eb666",
+    "url": "/rss-game/static/media/2.dce40399.jpg"
+  },
+  {
+    "revision": "6781752acf9eeefd0c25736bd3a3d567",
+    "url": "/rss-game/static/media/sprite.6781752a.png"
+  },
+  {
+    "revision": "b95c4552d81df640b044ff2f8e501d22",
+    "url": "/rss-game/static/media/sprite.b95c4552.png"
+  },
+  {
+    "revision": "3d7f26a876a572f30b0f",
+    "url": "/rss-game/static/js/main.3d7f26a8.chunk.js"
+  },
+  {
+    "revision": "1e2bc7aca51dac68e3ef743d9de9f866",
+    "url": "/rss-game/static/media/sprite.1e2bc7ac.png"
+  },
+  {
+    "revision": "80c764a6d9eadaa4134d6eb06c7a22ee",
+    "url": "/rss-game/static/media/sprite.80c764a6.png"
+  },
+  {
+    "revision": "b28ddce0657e50fa826a35de051a456a",
+    "url": "/rss-game/static/media/sprite.b28ddce0.png"
+  },
+  {
+    "revision": "dccb316bdf9560ed175a407bb569d9d4",
+    "url": "/rss-game/static/media/sprite.dccb316b.png"
+  },
+  {
+    "revision": "cac937b2d035acdfff7cd623ee44f1ed",
+    "url": "/rss-game/static/media/sprite.cac937b2.png"
+  },
+  {
+    "revision": "449cc276caa311d52ea6d1e42433d6ad",
+    "url": "/rss-game/index.html"
+  }
+];
